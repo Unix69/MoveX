78fbd34bd046b0a0f66b5c6857210b4c891b4df1
giuseppecanto <giuseppecanto1993@gmail.com>
2018-10-06 14:05:55 +0200

    Trying to modify from Visual Studio

--- Movex.FTP/UploadChannelInfo.cs
+++ Movex.FTP/UploadChannelInfo.cs
@@ -23,8 +23,8 @@ namespace Movex.FTP
             mEnd_index = end_index;
         }
 
-        public UploadChannelInfo(ref UploadChannel uchan) {
-            mUchan = uchan;
+        public UploadChannelInfo(ref UploadChannel uchans) {
+            mUchan = uchans;
         }
 
         public void Switch_upload() {
